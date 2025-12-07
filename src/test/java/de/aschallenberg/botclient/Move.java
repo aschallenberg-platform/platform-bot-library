8873c66b0210800ce5cded2e5861daae934d22ed
aschallenberg <alexandermarkusschallenberg@gmail.com>
2025-12-07 17:51:07 +0100

    Updated

--- src/test/java/de/aschallenberg/botclient/Move.java
+++ src/test/java/de/aschallenberg/botclient/Move.java
@@ -8,4 +8,20 @@ import java.io.Serializable;
 public class Move implements Serializable {
 	private int[] board;
 	private int player;
+
+	public int[] getBoard() {
+		return board;
+	}
+
+	public void setBoard(final int[] board) {
+		this.board = board;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(final int player) {
+		this.player = player;
+	}
 }
